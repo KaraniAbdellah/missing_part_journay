77f0cc3f21c2f35996584d9da9907263e6c753a7
KaraniAbdellah <abdellahkarani@gmail.com>
2025-07-24 23:02:13 +0100

    add how to commit rules

--- how_commit.js
+++ how_commit.js
@@ -2,4 +2,23 @@
 
 /*
     src: https://dev.to/ritikbanger/how-to-write-git-commit-messages-like-a-pro-dn
+
+    the commit should follow this convention:
+        <type>(<scope>): <message>
+    
+    --> for type can be:
+        feat: for a new feature.
+        refactor: for refactoring production code, e.g. renaming a function.
+        docs: for changes to the documentation.
+        fix: for a bug fix for the user.
+        perf: for performance improvements.
+        style: for formatting changes, missing semicolons, etc.
+        test: for adding missing tests, refactoring tests.
+        build: for updating build configuration, development tools or other changes irrelevant to the user.
+
+        
+
 */
+
+
+
