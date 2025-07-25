6e0ce81b763117295da9624f271d38978cbffcb5
KaraniAbdellah <abdellahkarani@gmail.com>
2025-07-25 23:37:14 +0100

    docs: add new srcs and point

--- avoid_legacy_code.js
+++ avoid_legacy_code.js
@@ -1,1 +1,12 @@
 // How to Avoid Legacy Code
+
+/*
+    src: https://www.youtube.com/watch?v=xmDAYE5o2LI
+    src: https://www.youtube.com/watch?v=_v0ghxbncoY
+    src: https://www.youtube.com/@CoderOne
+
+    POINT: 
+        class component
+        redux
+        typescript
+*/
